dc7ce91ca65d3b50af3939f1018b1e4866c9fcc0
李睿 <liruiliuye@163.com>
2019-11-20 18:34:07 +0800

    第二次提交，更新了在获取所有数据情况下，当数据过多时的翻页功能。

--- src/action/DaoTest.java
+++ src/action/DaoTest.java
@@ -6,7 +6,8 @@ import java.util.List;
 public class DaoTest {
 
 	public static void main(String[] args) {
-	
+
+		//System.out.println(6%3);
 		UiDesign ui = new UiDesign();
 	}
 	
--- src/action/UiDesign.java
+++ src/action/UiDesign.java
@@ -11,11 +11,16 @@ import javax.swing.*;
 
 public class UiDesign extends JFrame{
 
-	JPanel jpName,jpPosition,jpSalary,jpButton,jpDelect,jpDisplay,jpPath;
-	JLabel jlId,jlName,jlPosition,jlSalary,jlDisplay,jlPath;
+	JPanel jpName,jpPosition,jpSalary,jpButton,jpDelect,jpDisplay,jpPath,jpPage;
+	JLabel jlId,jlName,jlPosition,jlSalary,jlDisplay,jlPath,jlPage;
 	JTextField jtId,jtName,jtPosition,jtSalary,jtPath;
-	JButton jbAdd,jbDelect,jbUpdata,jbFindOne,jbFindAll,jbPath;
+	JButton jbAdd,jbDelect,jbUpdata,jbFindOne,jbFindAll,jbPath,jbup,jbdown;
 	EmployeeDao empDao =null ;
+	int count = 0;
+	int currentCount = 0;
+	int reCurrentCount = 0;
+	boolean canPage = false;
+	List<Employee> list = null;
 	
 	public UiDesign() {
 		jpName = new JPanel();
@@ -25,6 +30,7 @@ public class UiDesign extends JFrame{
 		jpDelect = new JPanel();
 		jpDisplay = new JPanel();
 		jpPath = new JPanel();
+		jpPage = new JPanel();
 		
 		jlName = new JLabel("姓名：");
 		jlPosition = new JLabel("职位：");
@@ -32,15 +38,21 @@ public class UiDesign extends JFrame{
 		jlDisplay = new JLabel("无");
 		jlId = new JLabel("ID");
 		jlPath = new JLabel("文件存储路径：");
+		jlPage = new JLabel("只有1页");
 		
 		jtName = new JTextField(10);
 		jtPosition = new JTextField(10);
 		jtSalary = new JTextField(10);
 		jtId = new JTextField(7);
 		jtPath = new JTextField(15);
+
+		//jtPath.setText("D:/Project/Eclipes/IOStreamCURD/data.txt");
 		
 		jbAdd = new JButton("增加");
+		jbup = new JButton("上一页");
+		jbdown = new JButton("下一页");
 		jbAdd.addActionListener(new ActionListener() {
+			@Override
 			public void actionPerformed(ActionEvent e) {
 				if(empDao==null) {
 					jlDisplay.setText("请先设置路径");
@@ -52,14 +64,17 @@ public class UiDesign extends JFrame{
 						emp.setSalary(Double.parseDouble(jtSalary.getText()));
 						empDao.save(emp);
 		                jlDisplay.setText("添加成功");
+		                jlPage.setText("共1页");
 					}
 				}
             }
 		});
 		jbDelect = new JButton("删除");
 		jbDelect.addActionListener(new ActionListener() {
+			@Override
 			public void actionPerformed(ActionEvent e) {
 				int id = 0;
+				jlPage.setText("共1页");
 				if(empDao==null) {
 					jlDisplay.setText("请先设置路径");
 				}else {
@@ -80,7 +95,9 @@ public class UiDesign extends JFrame{
 		});
 		jbUpdata = new JButton("更新");
 		jbUpdata.addActionListener(new ActionListener() {
+			@Override
 			public void actionPerformed(ActionEvent e) {
+				jlPage.setText("共1页");
 				if(empDao==null) {
 					jlDisplay.setText("请先设置路径");
 				}else {
@@ -102,7 +119,9 @@ public class UiDesign extends JFrame{
 		});
 		jbFindOne = new JButton("获取一个");
 		jbFindOne.addActionListener(new ActionListener() {
+			@Override
 			public void actionPerformed(ActionEvent e) {
+				jlPage.setText("共1页");
 				if(empDao==null) {
 					jlDisplay.setText("请先设置路径");
 				}else {
@@ -121,19 +140,57 @@ public class UiDesign extends JFrame{
 				}
 			}
 		});
+		//翻页用
+		jbdown.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if(canPage){
+					if(currentCount>count){
+						display(0);
+						jlPage.setText("第"+(count+1)+"页，共"+(currentCount+1)+"页");
+						count++;
+						//jlDisplay.setText("1");
+					}else {
+						if(reCurrentCount>0){
+							display(1);
+							jlPage.setText("第"+(count+1)+"页，共"+(currentCount+1)+"页");
+							//jlDisplay.setText(reCurrentCount+" ");
+							//count++;
+						}
+					}
+				}
+			}
+		});
+		//翻页用
+		jbup.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if(canPage){
+					count--;
+					display(0);
+					jlPage.setText("第"+(count+1)+"页，共"+(currentCount+1)+"页");
+					System.out.println(count);
+					if(count==0){
+						count=1;
+					}
+				}
+			}
+		});
 		jbFindAll = new JButton("获取所有");
 		jbFindAll.addActionListener(new ActionListener() {
+			@Override
 			public void actionPerformed(ActionEvent e) {
+				count=0;
 				if(empDao==null) {
 					jlDisplay.setText("请先设置路径");
-				}else {
-					String text = "<html>";
-					List<Employee> list = empDao.findAll(jlDisplay);
-					for(int i=0;i<list.size();i++) {
-						text = text + "ID:"+list.get(i).getId()+"...."+"姓名："+list.get(i).getName()+"...."+"职位："+list.get(i).getPosition()+"...."+"工资："+list.get(i).getSalary()+"<br>";
-					}
-					text = text+"<html>";
-					jlDisplay.setText(text);
+					}else {
+						list = empDao.findAll(jlDisplay);
+						currentCount = list.size()/3;
+						reCurrentCount = list.size()%3;
+						display(0);
+					jlPage.setText("第"+(count+1)+"页，共"+(currentCount+1)+"页");
+						count++;
+						canPage = true;
 					}
 				}
 			}
@@ -141,13 +198,17 @@ public class UiDesign extends JFrame{
 		
 		jbPath = new JButton("确定路径");
 		jbPath.addActionListener(new ActionListener(){
+			@Override
 			public void actionPerformed(ActionEvent e) {
+				jlPage.setText("共1页");
 				empDao = new EmployeeDao(jtPath.getText());
 				jlDisplay.setText("路径成功设置为："+jtPath.getText());
 			}
 		});
-		this.setLayout(new GridLayout(7, 1));
-		
+		this.setLayout(new GridLayout(8, 1));
+
+
+
 		jpPath.add(jlPath);
 		jpPath.add(jtPath);
 		jpPath.add(jbPath);
@@ -169,6 +230,10 @@ public class UiDesign extends JFrame{
 		jpDelect.add(jbDelect);
 		jpDelect.add(jbFindOne);
 		jpDelect.add(jbUpdata);
+
+		jpPage.add(jbup);
+		jpPage.add(jlPage);
+		jpPage.add(jbdown);
 		
 		jpDisplay.add(jlDisplay);
 		
@@ -179,6 +244,7 @@ public class UiDesign extends JFrame{
 		this.add(jpButton);
 		this.add(jpDelect);
 		this.add(jpDisplay);
+		this.add(jpPage);
 		
 		this.setTitle("增删改查");
         this.setSize(600, 550);
@@ -202,4 +268,25 @@ public class UiDesign extends JFrame{
 		}
 		return result;
 	}
+
+
+	private void display(int reCount){
+		String text = "<html>";
+		if(reCount==0){
+			for(int i=0+(count*3);i<3+(count*3);i++) {
+				text = text + "ID:"+list.get(i).getId()+"...."+"姓名："+list.get(i).getName()+"...."+"职位："+list.get(i).getPosition()+"...."+"工资："+list.get(i).getSalary()+"<br>";
+			}
+			text = text+"<html>";
+			jlDisplay.setText(text);
+		}else {
+			for(int i=3+((count-1)*3);i<list.size();i++) {
+				text = text + "ID:"+list.get(i).getId()+"...."+"姓名："+list.get(i).getName()+"...."+"职位："+list.get(i).getPosition()+"...."+"工资："+list.get(i).getSalary()+"<br>";
+			}
+			text = text+"<html>";
+			jlDisplay.setText(text);
+		}
+
+	}
+
+
 }
